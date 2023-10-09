5d3839a02b1e7a924aad516370289e551d168d41
ochosteve <ochosteve08@gmail.com>
2023-10-09 12:41:19 +0100

    replace fetch with axios

--- server/model/apartment.model.js
+++ server/model/apartment.model.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const apartmentSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    // userId: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "User",
+    //   required: true,
+    // },
     title: {
       type: String,
       required: true,
